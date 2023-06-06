4ede7489b12f7a8a6c108f139c09e07a9d3b3e5e
User <egemberdievasafia@gmail.com>
2023-06-06 14:14:57 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         for (int firstCounter = 1; firstCounter <= 10; firstCounter++) {
             System.out.println(" ");
             for (int secondCounter = 1; secondCounter <= 10; secondCounter++) {
                 System.out.println(firstCounter+ " * " +secondCounter+" = "+firstCounter*secondCounter);
             }
+            System.out.println();
         }
       /*  int d = sc.nextInt();
         if (d == 1) {
